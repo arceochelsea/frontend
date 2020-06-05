6e7f55f6d42f6e8914b14086f9272f83f2d4d559
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-06-05 00:44:20 -0400

    Completed Step 4 and displayed 1st user on DOM

--- xhrapi.js
+++ xhrapi.js
@@ -11,8 +11,10 @@ function loadText() {
 
 
     // troubleshooting
+    console.log('1. xhr = new');
     console.log(xhr.readyState); //should == 4
     console.log(xhr.status); // 200 - 299
+    console.log('--------------');
 
     //2. step number 2 
     // initializes a request https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/open
@@ -21,10 +23,15 @@ function loadText() {
         //method The HTTP request method to use, such as "GET", "POST", "PUT", "DELETE", etc. Ignored for non-HTTP(S) URLs.
 
 
-    xhr.open('GET', 'fakeurldata.txt', true); //if steps 3 and 4 are successful the url data will be stored in xhr.responseText
+    xhr.open('GET', 'https://api.github.com/users', true); //if steps 3 and 4 are successful the url data will be stored in xhr.responseText
+    //must have https 
 
+
+    //troubleshooting
+    console.log('2. xhr.open');
     console.log(xhr.readyState); //should == 4
     console.log(xhr.status); // 200 - 299
+    console.log('--------------');
 
 
     //3. Step number 3
@@ -32,12 +39,35 @@ function loadText() {
 
     
     xhr.onload = function() {
-        if (this.readyState === 4 && this.status ) {
+        if (this.readyState === 4 && this.status === 200) {
+            const responseJSON = JSON.parse(this.responseText);
+            console.log(responseJSON);
+            let html = `<ul><li>${responseJSON[0].login}</li></ul>`; //
+            document.getElementById('dataOutput').innerHTML = html;
+
+
+
+            //troubleshooting
+            console.log('3. xhr.unload')
             console.log(xhr.readyState);
             console.log(xhr.status);
+            console.log('--------------');
+
+        } else if (this.status === 404) {
+            document.getElementById('dataOutput').innerText = '404 Error: File Not Found';
         }
 
     }
+
+    //4. Step Number 4
+    
+    xhr.send();
+
+    //troubleshooting
+    console.log('4. xhr.send');
+    console.log(xhr.readyState);
+    console.log(xhr.status);
+    console.log('--------------');
 }
 
 });
