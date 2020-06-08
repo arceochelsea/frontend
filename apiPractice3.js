6b0e4b1306e3257c5072c7f97886c2e892db968b
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-06-08 15:43:14 -0400

    XML practice with StarWars API with finished instruction

--- apiPractice3.js
+++ apiPractice3.js
@@ -1,51 +1,38 @@
-//first is to add eventlistener to JS
-
-document.addEventListener('DOMContentLoader', function () {
-
-//add an eventlistener that listens and does something when triggered
+document.addEventListener('DOMContentLoaded', function () {
+    
 document.getElementById('button').addEventListener('click', loadText);
-
-
-//create a function that will load the text
 
 function loadText() {
 
-
-//next you must create a new XMLHttpRequest() that is saved in a variable that can not be changed!
-
     const req = new XMLHttpRequest();
 
-    //next you must initialize this using the open method
-
     req.open('GET', 'https://swapi.dev/api/people/1/', true);
-
-    //afterwards you use the send method to send the request
-
+    
+    req.onload = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            const responseJSON = JSON.parse(this.responseText);
+            document.getElementById('dataOutput').innerHTML = JSON.stringify(responseJSON);
+        } else if (this.status === 404) {
+            document.getElementyById('dataOutput').innerHTML = '404 Error: File Not Found';
+        }
+    }
+    
     req.send();
 
-    
-
-
-
-    }
+    };
 
 });
 
-//next inside of the {} we want to display what happens for the onclick function inside of the DOM
 
-
-            //step 1 XMLHttpRequest object is created and saved in the req variable
-
-
-            //step 2 the open method initalizes a request.
-            //since it is GETTING info/data from API then it is 'GET'
-            //'/json/cats.json' is the API URL
-            //3rd argument is a Boolean value. setting it to TRUE makes it an asynchronous request.
-
-
-            //step 3 the send method sends the request
-
-
-            //step 4 the onload event handler parses the returned data and applies the JSON.stringify method to convert the JS object into a string.
-            //that string is then inserted as the message text. 
-
+//first is to add eventlistener and create a function
+//add an eventlistener that listens and does something when triggered
+//create a function that will load the text 
+//next you must create a new XMLHttpRequest() that is saved in a variable that can not be changed
+//next you must initialize this using the open method
+//then you must create another function that will equal the name of the variable and onload. onload is the function when it is successful! 
+//then you must determine what you want it to do!
+//if XMLHttpRequest is successful then what do you want it to do?
+//how do we know it is successful? 
+//next we must json parse it and then document it on the DOM
+//and what will happen if it doesnt work??
+//afterwards you use the send method to send the request
